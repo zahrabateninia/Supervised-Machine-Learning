efac4b28e484644ad374b090a519f0edd7232b75
zahrabateninia <zahrabatenin@gmail.com>
2025-05-13 15:22:03 +0330

    Train the model using LinearRegression

--- predict_house_price.py
+++ predict_house_price.py
@@ -5,6 +5,8 @@
 import pandas as pd
 from sklearn.datasets import fetch_california_housing
 from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+
 
 # Load California housing data
 data = fetch_california_housing()
@@ -22,3 +24,11 @@ y = df['Target']
 
 # Split data (80% for training, 20% for testing)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+
+# Create the model
+model = LinearRegression()
+
+# Train it using the training data
+model.fit(X_train, y_train)
+
